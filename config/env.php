13339076e48e225346f852510ec3450dd4531ecd
dickygeraldi <dickygeraldi@gmail.com>
2019-12-08 22:41:19 +0700

    Delete env credentials account

--- config/env.php
+++ config/env.php
@@ -1,11 +1,11 @@
 <?php
     $variaeble = [
         'DB_NAME' => 'testFlip',
-        'DB_USER' => 'kid1412d',
-        'DB_PASS' => 'INTELcoreI321#',
+        'DB_USER' => '',
+        'DB_PASS' => '#',
         'DB_HOST' => 'localhost',
         'BASE_URL_DISBURSEMENT' => 'https://nextar.flip.id',
-        'HEADER_AUTH_FLIP' => 'HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41',
+        'HEADER_AUTH_FLIP' => '',
         'ENDPOINT_DISBURSE' => '/disburse',
     ];
 
